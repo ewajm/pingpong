28b5c80f0d7a046b7d27ad1beed805624725ca41
Ewa Manek <manekej@gmail.com>
2016-08-19 10:08:40 -0700

    Add clear button and functionality for different word select

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,27 @@
 //<!-- Back End -->
-var checkNum = function(number){
+var checkNum = function(number, sillyWords){
+  console.log("this is " + sillyWords);
   if(number % 15 === 0){
-    return "pingpong";
+    return sillyWords[0] + sillyWords[1];
   } else if(number % 5 === 0){
-    return "pong";
+    return sillyWords[1];
   } else if(number % 3 === 0){
-    return "ping";
+    return sillyWords[0];
   } else {
     return number;
   }
 }
 
-var pingPong = function(topNum){
+var pingPong = function(topNum, sillyWords){
   var pingPongArray =[];
+  var toPush;
   if(topNum > 0){
     for(var i = 1; i <= topNum; i++){
-      pingPongArray.push(checkNum(i));
+      pingPongArray.push(checkNum(i, sillyWords));
     }
   } else {
     for(var i = -1; i >= topNum; i--){
-      pingPongArray.push(checkNum(i));
+      pingPongArray.push(checkNum(i, sillyWords));
     }
   }
   return pingPongArray;
@@ -27,13 +29,24 @@ var pingPong = function(topNum){
 
 //<!-- Front End  -->
 $(document).ready(function(){
+
   var sillyWordArray = [["ping", "pong"], ["fiddle", "faddle"], ["hoity", "toity"], ["hunky", "dory"], ["boogie", "woogie"], ["jiggery", "pokery"]];
+  //create select box
   for(var i = 0; i < sillyWordArray.length; i++){
     $("select#sillyWord").append("<option value='" + i + "'>" + sillyWordArray[i][0] + " " + sillyWordArray[i][1] + "</option>");
   }
+  //process basic ping pong form
   $("form#inputForm").submit(function(event){
     event.preventDefault();
-    var topNum = parseInt($("input#input").val());
-    $("div#result").text(pingPong(topNum));
+    var topNum = parseInt($("input#numInput").val());
+    var sillyWordIndex = parseInt($("select#sillyWord").val());
+    $("div#result").text(pingPong(topNum, sillyWordArray[sillyWordIndex]));
   });
+
+  $("#clear").click(function(event){
+    event.preventDefault();
+    $("form#inputForm")[0].reset();
+    $("div#result").text('');
+  });
+
 });

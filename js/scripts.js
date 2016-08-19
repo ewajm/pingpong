e854ecea7c4670ed60a3259d9b04e91afa334868
Ewa Manek <manekej@gmail.com>
2016-08-19 08:33:44 -0700

    Add logic for specs 3 & 4: replace numbers divisible by 5 and 15 with pong

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,14 @@
 var pingPong = function(topNum){
   var pingPongArray =[];
   for(var i = 1; i <= topNum; i++){
-    if(i % 3 === 0){
+    if(i % 15 === 0){
+      pingPongArray.push("pingpong");
+    } else if(i % 5 === 0){
+      pingPongArray.push("pong");
+    } else if(i % 3 === 0){
       pingPongArray.push("ping");
     } else {
-    pingPongArray.push(i);
+      pingPongArray.push(i);
     }
   }
   return pingPongArray;

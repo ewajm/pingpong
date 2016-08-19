fa829d37173023dc0af55da7b52ab069fb07c0bc
Ewa Manek <manekej@gmail.com>
2016-08-19 14:16:49 -0700

    Format game output properly

--- js/scripts.js
+++ js/scripts.js
@@ -41,15 +41,7 @@ $(document).ready(function(){
     var topNum = parseInt($("input#numInput").val());
     var wordsIndex = parseInt($("select#wordSelect").val());
     var outputArray = pingPong(topNum, scrollArray[wordsIndex]);
-    for(var i = 0; i < outputArray.length; i++){
-      var resultClass;
-      if(!parseInt(outputArray[i])) {
-        resultClass = "silly"
-      } else {
-        resultClass = "number"
-      }
-      $("div#result").append("<span class='output " + resultClass + " '>" + outputArray[i] + "<span>");
-    }
+    createNethackDisplay(outputArray);
     $("div#result").show();
   });
 
@@ -63,6 +55,30 @@ $(document).ready(function(){
   function generateSelect(array){
     for(i = 0; i < array.length; i++){
       $("select#wordSelect").append("<option value='" + i + "'>" + array[i][0] + " " + array[i][1] + "</option>");
+    }
+  }
+
+  function createNethackDisplay(array){
+    var htmlString = '<p>You read "';
+    for(var i = 0; i < array.length; i++){
+      if(i > 0){
+        htmlString+= " ";
+      }
+      htmlString += (array[i]);
+    }
+    htmlString += '" <button class="btn btn-default">More</button></p>';
+    $("#result").addClass("nethackResult").removeClass("normalResult").append(htmlString);
+  }
+
+  function createRegularDisplay(array){
+    for(var i = 0; i < outputArray.length; i++){
+      var resultClass;
+      if(!parseInt(outputArray[i])) {
+        resultClass = "silly"
+      } else {
+        resultClass = "number"
+      }
+      $("div#result").append("<span class='output " + resultClass + " '>" + outputArray[i] + "<span>");
     }
   }
 

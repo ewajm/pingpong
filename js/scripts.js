734f44a6b31b194c329a8e8abc7ab15379de3736
Ewa Manek <manekej@gmail.com>
2016-08-19 09:10:21 -0700

    Refactor code so that the ping, pong and pingpong checks are in a separate function

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,25 @@
 //<!-- Back End -->
+var checkNum = function(number){
+  if(number % 15 === 0){
+    return "pingpong";
+  } else if(number % 5 === 0){
+    return "pong";
+  } else if(number % 3 === 0){
+    return "ping";
+  } else {
+    return number;
+  }
+}
+
 var pingPong = function(topNum){
   var pingPongArray =[];
   if(topNum > 0){
     for(var i = 1; i <= topNum; i++){
-      if(i % 15 === 0){
-        pingPongArray.push("pingpong");
-      } else if(i % 5 === 0){
-        pingPongArray.push("pong");
-      } else if(i % 3 === 0){
-        pingPongArray.push("ping");
-      } else {
-        pingPongArray.push(i);
-      }
+      pingPongArray.push(checkNum(i));
     }
   } else {
     for(var i = -1; i >= topNum; i--){
-      if(i % 15 === 0){
-        pingPongArray.push("pingpong");
-      } else if(i % 5 === 0){
-        pingPongArray.push("pong");
-      } else if(i % 3 === 0){
-        pingPongArray.push("ping");
-      } else {
-        pingPongArray.push(i);
-      }
+      pingPongArray.push(checkNum(i));
     }
   }
   return pingPongArray;
